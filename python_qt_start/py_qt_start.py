6472d8e803b60d6bc96eda7ea24bfcf237651b18
ToLoW-56 <161954830+ToLoW-56@users.noreply.github.com>
2024-05-18 18:33:51 +0300

    Add files via upload

---
+++ python_qt_start/py_qt_start.py
@@ -0,0 +1,74 @@
+import json
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, \
+    QHBoxLayout, QVBoxLayout, QFormLayout
+
+app = QApplication([])
+notes = []
+
+'''Інтерфейс'''
+# Параметри вікна
+notes_win = QWidget()
+notes_win.setWindowTitle('Розумні нотатки')
+notes_win.resize(900, 600)
+
+# Віджети
+list_notes = QListWidget()
+list_notes_label = QLabel('Список нотаток')
+
+button_note_create = QPushButton('Створити нотатку')
+button_note_del = QPushButton('Видалити нотатку')
+button_note_save = QPushButton('Зберегти нотатку')
+
+field_tag = QLineEdit('')
+field_tag.setPlaceholderText('Ввести тег...')
+field_text = QTextEdit()
+button_tag_add = QPushButton('Додати до нотатки')
+button_tag_del = QPushButton('Видалити з нотатки')
+button_tag_search = QPushButton('Шукати нотатки за тегом')
+list_tags = QListWidget()
+list_tags_label = QLabel('Список тегів')
+
+# Розмітки віджетів
+layout_notes = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_1.addWidget(field_text)
+
+col_2 = QVBoxLayout()
+col_2.addWidget(list_notes_label)
+col_2.addWidget(list_notes)
+row_1 = QHBoxLayout()
+row_1.addWidget(button_note_create)
+row_1.addWidget(button_note_del)
+row_2 = QHBoxLayout()
+row_2.addWidget(button_note_save)
+col_2.addLayout(row_1)
+col_2.addLayout(row_2)
+
+col_2.addWidget(list_tags_label)
+col_2.addWidget(list_tags)
+col_2.addWidget(field_tag)
+row_3 = QHBoxLayout()
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
+row_4 = QHBoxLayout()
+row_4.addWidget(button_tag_search)
+
+col_2.addLayout(row_3)
+col_2.addLayout(row_4)
+
+layout_notes.addLayout(col_1, stretch=2)
+layout_notes.addLayout(col_2, stretch=1)
+notes_win.setLayout(layout_notes)
+
+def save_notes_to_json():
+    with open("notes_data.json", "w") as file:
+        json.dump(notes, file, indent=4, ensure_ascii=False, sort_keys=True)
+# Старт застосунку
+notes_win.show()
+
+# Додати всі з json до list_notes для подальшої роботи
+for note in notes:
+    list_notes.addItem(note['name'])
+
+app.exec_()
